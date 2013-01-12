5a920e3f8bef5b4a33cba278d0396daab65669be
yababay <yababay@pochta.ru>
2013-01-12 04:48:15 +0000

    ByloStalo

--- Main.java
+++ Main.java
@@ -1,13 +1,28 @@
-public class Main { /* Hello, Katya!
+public class Main { 
+	
+/* 
+   Hello, Katya!
    Tram-pam-pam :)
 */
 
     public static void main(String[] args){
-    String s = getMessage();    
-    System.out.println(s);
+        String s = getMessage();    
+        System.out.println(s);
     }
     
-    private static String getMessage ({
+    /*
+     *  Bylo
+     */
+
+//    private static String getMessage ({
+//        return "Hello from metod!";
+//})
+    
+    /*
+     *  Nado tak
+     */
+    
+    private static String getMessage (){
         return "Hello from metod!";
-})
+    }
 } 
