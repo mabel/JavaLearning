927373639329bd50f87dcfa716a818c4e98d49f5
Gary76 <banzay1994@yandex.ru>
2013-01-15 11:50:55 -0800

    myerrors

--- Main.java
+++ Main.java
@@ -4,15 +4,30 @@ public class Main {
         String s = getMessage();    
         System.out.println(s +" "+args[0] + " "+args[1]);
         /* Цикл внутри метода */
-        for(int i = 0; i < 10 i++){
-            System.outprintln("i = "+i);
+        for(int i = 0; i < 10; i+=2){
+            System.out.println("i = "+i);
         }
     }
-    
-    private static String getMessage (){
+
+        int count = 0;
+        String s = "Hello! ";
+        while(count < 10){
+            s += s;
+            if(s.length > 30) break;
+       	    count++;
+        }
+        System.out.printl(s);
+        System.out.println.(count)
+        private static String getMessage (){
         return "Hello from metod!";
     }
 }
+
+
+
+
+
+
 
 
 
